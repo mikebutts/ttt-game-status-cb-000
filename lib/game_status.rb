294cea6184439e1231cc55f94b0df1db0082b4aa
Michael Butts <exodus8465@hotmail.com>
2019-02-14 01:43:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,14 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8], # bottom row
+  [0,3,6], # first vetical
+  [1,4,7], #second vertical
+  [2,5,8], # thirt vertical
+  [0,4,8],  # diagonal
+  [2,4,6] #diagonal
+  # ETC, an array for each win combination
+]
